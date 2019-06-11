1f6020f03dd6be3e70d00dbbaece6b38a791cb45
joe192168 <joe192168@sina.com>
2019-06-11 11:12:37 +0800

    更改事务

--- springcloud-client/src/main/java/com/client/service/impl/OrderServiceImpl.java
+++ springcloud-client/src/main/java/com/client/service/impl/OrderServiceImpl.java
@@ -27,14 +27,14 @@ public class OrderServiceImpl implements OrderService {
     private ProductFeign productFeign;
 
     @Override
-    @LcnTransaction
+    @TccTransaction
     @Transactional
     public String saveOrder(Order order) {
         Product product = new Product();
         product.setProductId(order.getOrdersProductId());
         String ret = productFeign.modifyProductPiece(product); //修改商品数量
         orderMapper.saveOrder(order);//添加订单
-        //int i = 1 / 0;
+        int i = 1 / 0;
         return ret;
     }
 }
