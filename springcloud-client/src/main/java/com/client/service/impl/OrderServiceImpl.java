03d8ba136785dcd48a90b2d51457b61e2790ef36
joe192168 <joe192168@sina.com>
2019-06-14 19:06:20 +0800

    更改事务

--- springcloud-client/src/main/java/com/client/service/impl/OrderServiceImpl.java
+++ springcloud-client/src/main/java/com/client/service/impl/OrderServiceImpl.java
@@ -5,10 +5,8 @@ import com.client.dto.Product;
 import com.client.feign.ProductFeign;
 import com.client.mapper.OrderMapper;
 import com.client.service.OrderService;
-import com.codingapi.txlcn.tc.annotation.LcnTransaction;
-import com.codingapi.txlcn.tc.annotation.TccTransaction;
-import com.codingapi.txlcn.tc.annotation.TxcTransaction;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.servicecomb.pack.omega.context.annotations.SagaStart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,14 +25,14 @@ public class OrderServiceImpl implements OrderService {
     private ProductFeign productFeign;
 
     @Override
-    @TccTransaction
+    @SagaStart(timeout=10)
     @Transactional
     public String saveOrder(Order order) {
         Product product = new Product();
         product.setProductId(order.getOrdersProductId());
         String ret = productFeign.modifyProductPiece(product); //修改商品数量
         orderMapper.saveOrder(order);//添加订单
-        int i = 1 / 0;
+        //int i = 1 / 0;
         return ret;
     }
 }
